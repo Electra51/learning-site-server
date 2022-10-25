cf7d7f06608f7526b704de4ded6eeb2f5d3d69d1
Electra51 <safayetnurelectra@gmail.com>
2022-10-25 19:33:25 +0600

    category server

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const categories = require('./Data/categories.json');
+const courses = require('./Data/courses.json');
+
 
 app.get('/', (req, res) => {
     res.send('category running')
@@ -14,6 +16,28 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+});
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses);
+    }
+    else {
+        const category_courses = courses.filter(course => course.category_id === id);
+        res.send(category_courses);
+    }
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(course => course._id === id);
+    res.send(selectedCourses);
+    
+});
 
 app.listen(port, () => {
     console.log('server running on port',port)
